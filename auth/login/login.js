be2f95a71cf2fc0fd6467db767b5e1418f3ad8b8
Hamza Daniyal <hamza@Faisals-iMac.local>
2023-12-27 13:37:08 +0500

    updated version

--- auth/login/login.js
+++ auth/login/login.js
@@ -56,48 +56,6 @@ function LoginSubmitForm() {
         });
     }
 }
-function showToast(message, type) {
-    // Create the toast container
-    const toastContainer = document.createElement('div');
-    toastContainer.className = `toast align-items-center text-white border-0 position-absolute top-0 start-50 translate-middle-x bg-${type}`;
-    toastContainer.setAttribute('role', 'alert');
-    toastContainer.setAttribute('aria-live', 'assertive');
-    toastContainer.setAttribute('aria-atomic', 'true');
-    toastContainer.style.top = '5px';
-
-    // Create the toast content
-    const toastContent = document.createElement('div');
-    toastContent.className = 'd-flex';
-
-    const toastBody = document.createElement('div');
-    toastBody.className = 'toast-body';
-    toastBody.textContent = message;
-
-    const closeButton = document.createElement('button');
-    closeButton.type = 'button';
-    closeButton.className = 'btn-close btn-close-white me-2 m-auto';
-    closeButton.setAttribute('data-bs-dismiss', 'toast');
-    closeButton.setAttribute('aria-label', 'Close');
-
-    // Append elements
-    toastContent.appendChild(toastBody);
-    toastContent.appendChild(closeButton);
-    toastContainer.appendChild(toastContent);
-
-    // Append toast to the body
-    document.body.appendChild(toastContainer);
-
-    // Initialize Bootstrap Toast
-    const bootstrapToast = new bootstrap.Toast(toastContainer);
-
-    // Show the toast
-    bootstrapToast.show();
-
-    // Automatically dismiss the toast after 3 seconds
-    setTimeout(() => {
-        bootstrapToast.hide();
-    }, 3000);
-}
 
 
 
--- customers/customer.js
+++ customers/customer.js
@@ -1,11 +1,14 @@
 var customerDataList
-let userDetails;
 const itemsPerPage = 10;
 let totalPages;
 let currentPage = 1;
 let start = 1;
 let size = 10;
-
+window.onload = function () {
+    const noDataMessage = document.getElementById('noDataFound');
+    noDataMessage.style.display = 'none';
+    getCustomersData();
+};
 
 function populateTable() {
     const tbody = document.getElementById('customerTableBody');
@@ -118,11 +121,6 @@ function changePage(page) {
     }
 }
 
-function logout() {
-    localStorage.clear();
-    window.location.href = '/auth/login/login.html';
-}
-
 function getCustomersData() {
     apiService.getCustomers(start, size)
         .then(response => {
@@ -140,102 +138,4 @@ function getCustomersData() {
             showToast(error.message, 'danger');
         });
 }
-function getUserDetail(comingId) {
-    apiService.getUserDetail(comingId)
-        .then(response => {
-            if (response.status == 'failed') {
-                console.log(response.message);
-                showToast(response.message, 'danger');
-            } else {
-                updateProfilePictures(response.data[0])
-            }
-        })
-        .catch(error => {
-            showToast(error.message, 'danger');
-        });
-}
 
-function showToast(message, type) {
-    const toastContainer = document.createElement('div');
-    toastContainer.className = `toast align-items-center text-white border-0 position-absolute top-0 start-50 translate-middle-x bg-${type}`;
-    toastContainer.setAttribute('role', 'alert');
-    toastContainer.setAttribute('aria-live', 'assertive');
-    toastContainer.setAttribute('aria-atomic', 'true');
-    toastContainer.style.top = '5px';
-
-    const toastContent = document.createElement('div');
-    toastContent.className = 'd-flex';
-
-    const toastBody = document.createElement('div');
-    toastBody.className = 'toast-body';
-    toastBody.textContent = message;
-
-    const closeButton = document.createElement('button');
-    closeButton.type = 'button';
-    closeButton.className = 'btn-close btn-close-white me-2 m-auto';
-    closeButton.setAttribute('data-bs-dismiss', 'toast');
-    closeButton.setAttribute('aria-label', 'Close');
-
-    toastContent.appendChild(toastBody);
-    toastContent.appendChild(closeButton);
-    toastContainer.appendChild(toastContent);
-
-    document.body.appendChild(toastContainer);
-
-    const bootstrapToast = new bootstrap.Toast(toastContainer);
-    bootstrapToast.show();
-
-    setTimeout(() => {
-        bootstrapToast.hide();
-    }, 3000);
-}
-async function handleFileUpload(e) {
-    const fileInput = e.target;
-    let functionParam = {
-        userId: userDetails.id
-    }
-    const formData = new FormData();
-    formData.append('file', fileInput.files[0]);
-    formData.append('folder', 'public/images');
-    formData.append('makePublic', true);
-    formData.append('functionName', 'AddUserImage');
-    formData.append('functionParam', JSON.stringify(functionParam));
-    try {
-        // Assuming you have an apiService with an uploadImage method
-        const response = await apiService.uploadImage(formData);
-
-        // Update the image source if the upload is successful
-        if (response) {
-            updateProfilePictures(response)
-        } else {
-            showToast(response.message, 'danger');
-        }
-    } catch (error) {
-        showToast(error.message, 'danger');
-    }
-}
-function updateProfilePictures(response) {
-    const profilePic = document.getElementById('profilePic');
-    const profileIcon = document.getElementById('profileImg');
-    const newImageUrl = response.pictureUrl; // Replace with the actual key in the response
-    profilePic.src = newImageUrl;
-    profileIcon.src = newImageUrl
-}
-
-window.onload = function () {
-    const noDataMessage = document.getElementById('noDataFound');
-    noDataMessage.style.display = 'none';
-    userDetails = JSON.parse(localStorageService.getItem('userDetail'))
-    if (userDetails && userDetails.firstName) {
-        document.getElementById('userName').innerText = userDetails.firstName;
-    }
-    if (userDetails && userDetails.firstName && userDetails.lastName) {
-        document.getElementById('firstNameInput').value = userDetails.firstName;
-        document.getElementById('lastNameInput').value = userDetails.lastName;
-    }
-    getCustomersData();
-    var userIdAsString = userDetails.id.toString();
-
-    // Now, you can pass userIdAsString to the getUserDetail function
-    getUserDetail(userIdAsString);
-};
